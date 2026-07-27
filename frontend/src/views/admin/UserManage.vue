c9e0a429c4f9d7b8cffb5135180935ede54a9523
lham <523800546@qq.com>
2026-07-27 14:57:07 +0800

    fix: 一些前端页面样式的调整

--- frontend/src/views/admin/UserManage.vue
+++ frontend/src/views/admin/UserManage.vue
@@ -40,7 +40,7 @@
     </div>
     <FormDrawer v-model:visible="dialogVisible" :title="isEdit ? '编辑用户' : '新增用户'" :loading="submitting" @submit="handleSubmit">
       <el-form ref="formRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="用户名" prop="username"><el-input v-model="form.username" :disabled="isEdit" placeholder="搜索用户名或姓名" /></el-form-item>
+        <el-form-item label="用户名" prop="username"><el-input v-model="form.username" :disabled="isEdit" placeholder="请输入用户名" /></el-form-item>
         <el-form-item v-if="!isEdit" label="密码" prop="password"><el-input v-model="form.password" type="password" placeholder="请输入密码" show-password /></el-form-item>
         <el-form-item label="姓名"><el-input v-model="form.realName" placeholder="请输入真实姓名" /></el-form-item>
         <el-form-item label="手机号"><el-input v-model="form.phone" placeholder="请输入手机号" /></el-form-item>
